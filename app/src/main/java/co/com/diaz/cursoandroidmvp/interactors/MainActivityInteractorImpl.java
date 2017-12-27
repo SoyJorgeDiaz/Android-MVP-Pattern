0c0a028c99db4a1ba03f2992e725adb32a347c52
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-27 00:45:18 -0500

    Presenter definition

--- app/src/main/java/co/com/diaz/cursoandroidmvp/interactors/MainActivityInteractorImpl.java
+++ app/src/main/java/co/com/diaz/cursoandroidmvp/interactors/MainActivityInteractorImpl.java
@@ -1,8 +1,18 @@
 package co.com.diaz.cursoandroidmvp.interactors;
 
+import co.com.diaz.cursoandroidmvp.interfaces.IMainActivityInteractor;
+
 /**
  * Created by SoyJorgeDiaz on 27/12/2017.
  */
 
-public class MainActivityInteractorImpl {
+public class MainActivityInteractorImpl implements IMainActivityInteractor{
+
+    public MainActivityInteractorImpl() {
+    }
+
+    @Override
+    public void addText(String s) {
+
+    }
 }
--- app/src/main/java/co/com/diaz/cursoandroidmvp/presenters/MainActivityPresenterImpl.java
+++ app/src/main/java/co/com/diaz/cursoandroidmvp/presenters/MainActivityPresenterImpl.java
@@ -1,8 +1,31 @@
 package co.com.diaz.cursoandroidmvp.presenters;
 
+import co.com.diaz.cursoandroidmvp.interactors.MainActivityInteractorImpl;
+import co.com.diaz.cursoandroidmvp.interfaces.IMainActivityInteractor;
+import co.com.diaz.cursoandroidmvp.interfaces.IMainActivityPresenter;
+import co.com.diaz.cursoandroidmvp.interfaces.IMainActivityView;
+
 /**
  * Created by SoyJorgeDiaz on 27/12/2017.
  */
 
-public class MainActivityPresenterImpl {
+public class MainActivityPresenterImpl implements IMainActivityPresenter{
+
+    private IMainActivityView iView;
+    private IMainActivityInteractor iInteractor;
+
+    public MainActivityPresenterImpl(IMainActivityView iView) {
+        this.iView = iView;
+        iInteractor = new MainActivityInteractorImpl();
+    }
+
+    @Override
+    public void showResult(String s) {
+
+    }
+
+    @Override
+    public void addText(String s) {
+
+    }
 }
