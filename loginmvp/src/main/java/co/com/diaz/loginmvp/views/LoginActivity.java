03dcbc48155155d1939acc55435b882b66e08dd4
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-28 23:54:00 -0500

    Login activity refactor

---
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/views/LoginActivity.java
@@ -0,0 +1,30 @@
+package co.com.diaz.loginmvp.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import co.com.diaz.loginmvp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputUsername = findViewById(R.id.inputUsername);
+        inputPassword = findViewById(R.id.inputPassword);
+        progressBar = findViewById(R.id.progressbar);
+    }
+
+    public void eventLogin(View view) {
+    }
+}
