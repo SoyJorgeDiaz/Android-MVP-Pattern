e94089f8d00aa949f3e5981b2aa0ff96c4c23bfb
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-29 23:14:03 -0500

    Code of view components

--- loginmvp/src/main/java/co/com/diaz/loginmvp/views/LoginActivity.java
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/views/LoginActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import co.com.diaz.loginmvp.R;
+import co.com.diaz.loginmvp.interfaces.ILoginPresenter;
+import co.com.diaz.loginmvp.interfaces.ILoginView;
+import co.com.diaz.loginmvp.presenters.LoginPresenterImpl;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements ILoginView {
 
     private EditText inputUsername;
     private EditText inputPassword;
     private ProgressBar progressBar;
+    private ILoginPresenter iLoginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,32 @@ public class LoginActivity extends AppCompatActivity {
         inputUsername = findViewById(R.id.inputUsername);
         inputPassword = findViewById(R.id.inputPassword);
         progressBar = findViewById(R.id.progressbar);
+
+        iLoginPresenter = new LoginPresenterImpl(this);
     }
 
     public void eventLogin(View view) {
+        iLoginPresenter.attempLogin(inputUsername.getText().toString(),
+                inputPassword.getText().toString());
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void accessSuccessful() {
+        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void accessDenied() {
+        Toast.makeText(this, "Login access denied", Toast.LENGTH_SHORT).show();
     }
 }
