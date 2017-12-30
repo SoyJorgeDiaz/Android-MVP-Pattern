919168de8be77e5eb264a832f64f5937207eeeed
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-29 22:23:56 -0500

    New listener methods

--- loginmvp/src/main/java/co/com/diaz/loginmvp/interfaces/ILoginInteractor.java
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/interfaces/ILoginInteractor.java
@@ -1,9 +1,11 @@
 package co.com.diaz.loginmvp.interfaces;
 
+import co.com.diaz.loginmvp.listeners.IOnLoginFinishedListener;
+
 /**
  * Created by SoyJorgeDiaz on 28/12/2017.
  */
 
 public interface ILoginInteractor {
-    void validateCredentials(String username, String password);
+    void validateCredentials(IOnLoginFinishedListener listener, String username, String password);
 }
---
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/listeners/IOnLoginFinishedListener.java
@@ -0,0 +1,10 @@
+package co.com.diaz.loginmvp.listeners;
+
+/**
+ * Created by SoyJorgeDiaz on 29/12/2017.
+ */
+
+public interface IOnLoginFinishedListener {
+    void onError();
+    void onSuccess();
+}
