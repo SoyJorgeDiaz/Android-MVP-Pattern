300b4d6430f6b736dab5e4d03bf7f5f8919f5687
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-29 22:58:35 -0500

    Presenter listener

--- loginmvp/src/main/java/co/com/diaz/loginmvp/presenters/LoginPresenterImpl.java
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/presenters/LoginPresenterImpl.java
@@ -4,12 +4,13 @@ import co.com.diaz.loginmvp.interactors.LoginInteractorImpl;
 import co.com.diaz.loginmvp.interfaces.ILoginInteractor;
 import co.com.diaz.loginmvp.interfaces.ILoginPresenter;
 import co.com.diaz.loginmvp.interfaces.ILoginView;
+import co.com.diaz.loginmvp.listeners.IOnLoginFinishedListener;
 
 /**
  * Created by SoyJorgeDiaz on 29/12/2017.
  */
 
-public class LoginPresenterImpl implements ILoginPresenter {
+public class LoginPresenterImpl implements ILoginPresenter, IOnLoginFinishedListener {
 
     private ILoginView iLoginView;
     private ILoginInteractor iLoginInteractor;
@@ -21,6 +22,16 @@ public class LoginPresenterImpl implements ILoginPresenter {
 
     @Override
     public void attempLogin(String username, String password) {
+
+    }
+
+    @Override
+    public void onError() {
+        
+    }
+
+    @Override
+    public void onSuccess() {
 
     }
 }
