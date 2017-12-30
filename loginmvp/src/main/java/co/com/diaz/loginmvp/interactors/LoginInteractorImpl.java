ad43e8a0716a4aeb402a87188efcf7f97c838ba8
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-29 22:39:50 -0500

    Creating interactor and presenter

---
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/interactors/LoginInteractorImpl.java
@@ -0,0 +1,8 @@
+package co.com.diaz.loginmvp.interactors;
+
+/**
+ * Created by SoyJorgeDiaz on 29/12/2017.
+ */
+
+public class LoginInteractorImpl {
+}
---
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/presenters/LoginPresenterImpl.java
@@ -0,0 +1,25 @@
+package co.com.diaz.loginmvp.presenters;
+
+import co.com.diaz.loginmvp.interfaces.ILoginInteractor;
+import co.com.diaz.loginmvp.interfaces.ILoginPresenter;
+import co.com.diaz.loginmvp.interfaces.ILoginView;
+
+/**
+ * Created by SoyJorgeDiaz on 29/12/2017.
+ */
+
+public class LoginPresenterImpl implements ILoginPresenter {
+
+    private ILoginView iLoginView;
+    private ILoginInteractor iLoginInteractor;
+
+    public LoginPresenterImpl(ILoginView iLoginView) {
+        this.iLoginView = iLoginView;
+        iLoginInteractor = new LoginPresenterImpl();
+    }
+
+    @Override
+    public void attempLogin(String username, String password) {
+
+    }
+}
