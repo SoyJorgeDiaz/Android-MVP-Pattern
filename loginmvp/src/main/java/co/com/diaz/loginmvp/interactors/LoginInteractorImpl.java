7d8ea95632ceeb6f536ffaf4c60b783cb74b38fe
SoyJorgeDiaz <jlditru@gmail.com>
2017-12-29 22:50:34 -0500

    Interactor code logic

--- loginmvp/src/main/java/co/com/diaz/loginmvp/interactors/LoginInteractorImpl.java
+++ loginmvp/src/main/java/co/com/diaz/loginmvp/interactors/LoginInteractorImpl.java
@@ -1,8 +1,26 @@
 package co.com.diaz.loginmvp.interactors;
 
+import android.os.Handler;
+
+import co.com.diaz.loginmvp.interfaces.ILoginInteractor;
+import co.com.diaz.loginmvp.listeners.IOnLoginFinishedListener;
+
 /**
  * Created by SoyJorgeDiaz on 29/12/2017.
  */
 
-public class LoginInteractorImpl {
+public class LoginInteractorImpl implements ILoginInteractor {
+    @Override
+    public void validateCredentials(final IOnLoginFinishedListener listener, final String username, final String password) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (username.length() > 0 && password.length() > 0){
+                    listener.onSuccess();
+                }else {
+                    listener.onError();
+                }
+            }
+        }, 3000);
+    }
 }
